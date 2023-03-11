81976d7c3455d0e992e44cc3c9286bfea3a1e33c
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-11 15:32:03 -0700

    added some notes

--- src/utilities/String_Utility.java
+++ src/utilities/String_Utility.java
@@ -4,10 +4,10 @@ public class String_Utility {
 
     public static String reverse(String str){
 
-        String reverse = "";
+        String reverse = ""; // to contain all the characters of the given string in reverse
 
-        for (int i = str.length()-1;i >= 0; i--){  // if word/characters are unknown
-            reverse += str.charAt(i);
+        for (int i = str.length()-1;i >= 0; i--){  // if word/characters are unknown i: index number of the given string starting from last index to index 0
+            reverse += str.charAt(i); // to get each character of the string from last index to index zero
         }
 
         return reverse;
