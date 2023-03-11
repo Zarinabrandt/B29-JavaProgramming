2d9ec7de4fb87ebc8e119abdef7ca323213268e6
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-10 21:12:54 -0700

    day14 learning method overloading

---
+++ src/day14_For_Loop/Overloading_The_Main_Method.java
@@ -0,0 +1,27 @@
+package day14_For_Loop;
+
+public class Overloading_The_Main_Method {
+
+    public static void main(String[] args) {
+
+        System.out.println("A");
+
+    }
+
+
+    public static void main(int[] args){
+        System.out.println("B");
+
+    }
+
+    public static void main(double[] args){
+        System.out.println("C");
+
+    }
+
+    public static void main(boolean[] args){
+        System.out.println("D");
+
+    }
+
+}
