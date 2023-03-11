3492323ff68f1efa540af78ad5cbb2b1af012291
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-10 23:03:14 -0700

    day14 learning for loop

---
+++ src/day14_For_Loop/For_Loop_Intro.java
@@ -0,0 +1,16 @@
+package day14_For_Loop;
+
+public class For_Loop_Intro {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("-----------------------------------");
+
+
+        for (int i = 1;  i <= 10; i++){ // i: 1↻ 2↻ 3↻ 4↻ .... 10
+            System.out.println("Hello World"); // 1, 2, 3, 4 ....
+        }
+
+    }
+}
