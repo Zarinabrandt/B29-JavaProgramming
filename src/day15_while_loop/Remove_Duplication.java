964d4f251ab7676bea3ae5fc9237aa0e472d80cd
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-13 15:41:36 -0700

    day15 duplication

---
+++ src/day15_while_loop/Remove_Duplication.java
@@ -0,0 +1,30 @@
+package day15_while_loop;
+
+public class Remove_Duplication {
+    public static void main(String[] args) {
+
+        String str = "aabbbcccc";
+
+        String result = ""; //"abc"
+
+        for (int i = 0; i < str.length(); i++) { //i: index numbers of str
+            char each = str.charAt(i); //each character of the String str
+
+            if ( !result.contains( ""+ each) ){ // if the string result does not contain the character of string str yet
+                result += each; // then we will add the character to string result
+            }
+          // if the character is not contained yet in the new string, then we will add it to the new string
+        }
+        System.out.println(result);
+
+
+
+    }
+}
+/*
+3. Write a program that can remove the duplicated characters from a string
+				Ex:
+					str = "aabbbcccc"
+					output:
+						abc
+ */
