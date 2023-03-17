550e44e0664dd61a396b83a07e090a5e2cf996ac
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-17 16:49:18 -0700

    day17 loop

---
+++ src/day17_CustomClass/FrequencyOfCharacters.java
@@ -0,0 +1,46 @@
+package day17_CustomClass;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+        String str = "aabcccdddddzzzzzzxxxxx";
+        String result = ""; //a2b1c3d1
+                            //a b c d
+                            //2 1 3 1
+
+
+        for (int j = 0; j < str.length(); j++) {
+
+            char ch = str.charAt(j); //"a" needs to be compared with every single character or another each character
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == ch){
+                    count++;
+                } // from line 12-22 code count how many times character has been appeared
+            }
+
+            if (result.contains("" + ch)){ // if the character is already included in the result
+                continue; // skip that character
+            }
+
+            result += ch + "" + count;
+
+        }
+        System.out.println(result);
+
+
+
+
+
+
+
+
+    }
+}
+/*
+	2. Write a program that can find the frequency of the characters from a string
+                Ex:
+                        str = "aabcccd";
+                        output:
+                                a2b1c3d1
+ */
