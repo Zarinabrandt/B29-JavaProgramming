e8d7ea91db83f6c2825a40f2980f5074041e8635
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-18 13:47:26 -0700

    day17 class

---
+++ src/day17_CustomClass/Dog.java
@@ -0,0 +1,22 @@
+package day17_CustomClass;
+
+public class Dog {
+
+    public String name; //instance variable
+    public String breed;
+    public String size;
+    public int age;
+    public String color;
+
+
+    public void eat(){
+        System.out.println(name + " is eating dog food");
+    }
+
+    public void drink(){
+        System.out.println(name + " is drinking water");
+    }
+
+
+
+}
