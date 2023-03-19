915f4a6df475b10ccad46ff77e0ada8274e36ab7
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-18 17:43:21 -0700

    day17 class/adding

--- src/day17_CustomClass/Dog.java
+++ src/day17_CustomClass/Dog.java
@@ -9,6 +9,15 @@ public class Dog {
     public int age;
     public String color;
 
+    public void setInfo(String name,String breed,String size,char gender,int age,String color){ // this keyword is used for calling the instance variable name
+        this.name = name;
+        this.breed = breed;
+        this.size = size;
+        this.gender = gender;
+        this.age = age;
+        this.color = color;
+    }
+
 
     public void eat(){
         System.out.println(name + " is eating dog food");
@@ -18,6 +27,14 @@ public class Dog {
         System.out.println(name + " is drinking water");
     }
 
-
-
+    public String toString() { // to avoid a hash-codes when we print the class objects
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", size ='" + size + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
