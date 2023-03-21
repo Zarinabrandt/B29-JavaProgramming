c727b2a0aa05a69ece51bafce845806ac853ef4b
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-21 10:24:54 -0700

    added some code

--- src/day15_while_loop/While_Loop_Intro.java
+++ src/day15_while_loop/While_Loop_Intro.java
@@ -2,6 +2,18 @@ package day15_while_loop;
 
 public class While_Loop_Intro {
     public static void main(String[] args) {
+/*
+        int i = 10;
+        while(i <= 10){
+            System.out.println("I like java");
+            i++;
+            System.out.println("good to hear that");
+            i--;
+        } // it will print infinite times or until condition become false
+
+ */
+
+        System.out.println("-------------------------");
 
         if (true){
             System.out.println("Hello World");
@@ -18,7 +30,6 @@ public class While_Loop_Intro {
         while (true){
             System.out.println("Hello Cydeo"); // it will print infinite times or until condition become false
         }
-
 
 
     }
