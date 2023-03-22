63fb0ced53ce47d89a2ad7273c717093a936b59f
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-21 18:22:43 -0700

    day19 array

---
+++ src/day19_ArrayIntro/ArrayPractice1.java
@@ -0,0 +1,33 @@
+package day19_ArrayIntro;
+
+import java.util.Arrays;
+
+public class ArrayPractice1 {
+    public static void main(String[] args) {
+
+        String[] myGroup = new String[5]; // this line can be fixed, if change the number in array then we can assign more
+
+        myGroup[0] ="Gulchin";
+
+        //for last member of group we can assign as 4 or myGroup.length()
+        //myGroup[4] ="Asel";
+        myGroup[myGroup.length-1] = "Asel";
+        myGroup[2] ="Sumaye";
+        myGroup[1] ="Abdula";
+        myGroup[3] ="Nikita";
+
+        System.out.println(Arrays.toString(myGroup));
+
+        myGroup[1] ="Kuzzat";
+
+        System.out.println(Arrays.toString(myGroup));
+
+        System.out.println("--------------------------------------");
+
+        for (int i = myGroup.length-1; i >= 0; i--) {
+            System.out.println(myGroup[i]);
+        }
+
+
+    }
+}
