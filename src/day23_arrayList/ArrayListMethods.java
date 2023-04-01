827f4b056cc31ed33c58487e5be0998f91f87bbf
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-31 18:17:57 -0700

    day 23 array list methods

--- src/day23_arrayList/ArrayListMethods.java
+++ src/day23_arrayList/ArrayListMethods.java
@@ -103,6 +103,66 @@ public class ArrayListMethods {
         System.out.println("hasArina = " + hasArina);
 
 
+        System.out.println("----------------------------------------------------------------");
+
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = list1;
+
+        list1.add(10);
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        System.out.println(list1 == list2); //true
+
+
+        ArrayList<Integer> l1 = new ArrayList<>();
+        ArrayList<Integer> l2 = new ArrayList<>();
+
+        l1.add(10);
+        l2.add(10);
+
+
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(l1 == l2); //false
+
+        System.out.println(l1.equals(l2)); //true
+
+
+        System.out.println("----------------------------------------------------------------");
+
+
+        ArrayList<Integer> n1 = new ArrayList<>();
+        n1.add(10);
+        n1.add(20);
+        n1.add(30);
+
+        ArrayList<Integer> n2 = new ArrayList<>();
+        n2.add(30);
+        n2.add(10);
+        n2.add(20);
+
+
+        System.out.println("n1 = " + n1);
+        System.out.println("n2 = " + n2);
+
+        System.out.println(n1.equals(n2)); //false
+
+
+        System.out.println(n1.isEmpty()); //false
+        System.out.println(n2.isEmpty()); //false
+
+        n1.clear();
+        n2.clear();
+
+        System.out.println(n1.isEmpty()); //true
+        System.out.println(n2.isEmpty()); //true
+
+
+
 
 
 
