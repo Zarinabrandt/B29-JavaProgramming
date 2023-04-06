8747c734820c43634bf7c9c3b6841bdf62fd14a1
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-06 12:11:29 -0700

    time and dates

---
+++ src/day25_Constructors/LocalDateTimeIntro.java
@@ -0,0 +1,24 @@
+package day25_Constructors;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeIntro {
+    public static void main(String[] args) {
+
+        LocalDateTime starts = LocalDateTime.now();
+
+        System.out.println(starts + "❤");
+
+        LocalDateTime ends = LocalDateTime.of(2023,5,18,11,0);
+
+        System.out.println(ends+ "👍");
+
+        System.out.println(starts.getDayOfWeek());
+
+        System.out.println(starts.getHour());
+
+
+
+
+    }
+}
