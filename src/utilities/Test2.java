de3e369b5650b00d647b3870cb8151051d11d29d
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-12 13:03:47 -0700

    day27 static imports

---
+++ src/utilities/Test2.java
@@ -0,0 +1,42 @@
+package utilities;
+
+//import day27_AccessModifiers.Data;
+
+import day27_AccessModifiers.Data;
+
+//import static day27_AccessModifiers.Data.d;
+
+//import static day27_AccessModifiers.Data.method3;
+
+import static day27_AccessModifiers.Data.*; // * imports all the members from that class
+
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        //System.out.println(Data.d);
+
+        System.out.println(d);
+
+        method3();
+
+        System.out.println(e);
+        System.out.println(f);
+
+        method4();
+
+        /*
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        method1();
+        method2();
+
+         */
+
+
+    }
+
+}
