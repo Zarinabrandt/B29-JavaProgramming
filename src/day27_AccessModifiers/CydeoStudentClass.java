4ec1c360cd042ba0cd104c12bf1cdabc3af1e72d
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-12 14:02:03 -0700

    day27 static imports

---
+++ src/day27_AccessModifiers/CydeoStudentClass.java
@@ -0,0 +1,33 @@
+package day27_AccessModifiers;
+
+public class CydeoStudentClass {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public static String schoolName = "Cydeo";
+    public static String secretCode;
+
+    public CydeoStudentClass(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        //schoolName = "Cydeo School";
+        //secretCode = "Wooden Spoon";
+    }
+
+    static {
+       // schoolName = "Cydeo School";
+        secretCode = "Wooden Spoon";
+    }
+
+
+    public String toString() {
+        return "CydeoStudentClass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
