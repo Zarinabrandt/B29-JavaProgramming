429714bd963261a4e8a0dd35a571dc681093577d
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-12 13:42:35 -0700

    day27 static imports

---
+++ src/day27_AccessModifiers/Test.java
@@ -0,0 +1,14 @@
+package day27_AccessModifiers;
+
+public class Test {
+    public static void main(String[] args) {
+
+        new StaticInitializationBlock();
+
+        System.out.println(StaticInitializationBlock.a);
+        System.out.println(StaticInitializationBlock.b);
+        System.out.println(StaticInitializationBlock.c);
+
+
+    }
+}
