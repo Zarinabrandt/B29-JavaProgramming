8719064e104f8430eac516a0f3d4d6981812fe6d
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-18 17:20:09 -0700

    day29 inheritance

---
+++ src/day30_inheritance/phoneTask/Samsung.java
@@ -0,0 +1,17 @@
+package day30_inheritance.phoneTask;
+
+public class Samsung extends Phone{
+
+
+
+     public Samsung(String model, String size, String color, double price){
+          super("Samsung", model, size, color, price);
+     }
+
+     public void freeze(){
+          System.out.println(getBrand()+" "+getBrand()+ " freezes a lot");
+     }
+
+
+
+}
