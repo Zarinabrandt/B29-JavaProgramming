d1cb54a2c1f9f8dd2e369d86cf470b255855f295
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-27 12:01:00 -0700

    day32final keyword

---
+++ src/day32_finalKeyword/carTask_methodOverriding/Toyota.java
@@ -0,0 +1,15 @@
+package day32_finalKeyword.carTask_methodOverriding;
+
+public class Toyota extends Car{
+
+    public Toyota(String make, String model, String color, int year, double price) {
+        super(make, model, color, year, price);
+    }
+
+
+
+}
+/*
+  1. Toyota
+             start(): "Press the brake and twist the key to ignition"
+ */
