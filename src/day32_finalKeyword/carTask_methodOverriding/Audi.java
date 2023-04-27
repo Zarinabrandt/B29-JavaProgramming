34ef69ff6241e6253f208c189b076880f650c60f
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-27 12:11:07 -0700

    day32 final keyword

--- src/day32_finalKeyword/carTask_methodOverriding/Audi.java
+++ src/day32_finalKeyword/carTask_methodOverriding/Audi.java
@@ -8,7 +8,7 @@ public class Audi extends  Car{
 
     @Override
     public void start() {
-        System.out.println("Press the start button to start "+getMake()+getModel());
+        System.out.println("Press the start button to start "+getMake()+" "+getModel());
     }
 }
 /*
