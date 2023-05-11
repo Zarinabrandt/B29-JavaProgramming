9e880315dd7d53c22cd67db770e16ba85ec51961
antho <zarinabrandt@gmail.com>
2023-05-11 09:16:11 -0700

    day35

--- src/day35_polymorphism/transportationTask/CydeoCar.java
+++ src/day35_polymorphism/transportationTask/CydeoCar.java
@@ -1,8 +1,16 @@
 package day35_polymorphism.transportationTask;
 
-public class Tesla extends Car implements Electric, AutoPilot{
-    public Tesla(String make, String model, String color, int year, double price) {
+import day34_abstruaction.animalTask.Flyable;
+import day34_abstruaction.animalTask.Swimmable;
+
+public class CydeoCar extends Car implements Flyable, Electric, AutoPilot, Swimmable {
+    public CydeoCar(String make, String model, String color, int year, double price) {
         super(make, model, color, year, price);
+    }
+
+    @Override
+    public void fly() {
+
     }
 
     @Override
@@ -27,6 +35,11 @@ public class Tesla extends Car implements Electric, AutoPilot{
 
     @Override
     public void start() {
+
+    }
+
+    @Override
+    public void swim() {
 
     }
 }
