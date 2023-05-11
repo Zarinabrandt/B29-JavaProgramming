a386961fd2eae637a00cd3cb381caf262e2d041f
antho <zarinabrandt@gmail.com>
2023-05-11 09:00:31 -0700

    day35

--- src/day35_polymorphism/transportationTask/Plane.java
+++ src/day35_polymorphism/transportationTask/Plane.java
@@ -1,4 +1,13 @@
 package day35_polymorphism.transportationTask;
 
-public class Plane {
+public abstract class Plane extends Transportation{
+
+    public Plane(String make, String model, String color, int year, double price) {
+        super(make, model, color, year, price);
+    }
+
+    public void land(){
+        System.out.println("Plane "+getMake()+" "+getModel()+" is landing");
+    }
+
 }
