447e0962ab555c3c34a21e9003b15717873cf090
antho <zarinabrandt@gmail.com>
2023-05-20 12:18:47 -0700

    day39 collection, thread

--- src/day39_collections/ListDemo.java
+++ src/day39_collections/ListDemo.java
@@ -1,8 +1,6 @@
 package day39_collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ListDemo {
     public static void main(String[] args) {
@@ -29,6 +27,30 @@ public class ListDemo {
 
         System.out.println("----------------------------------------------------------------");
 
+        Vector<Integer> vector = new Vector<>();
+
+        vector.add(100);
+        vector.add(200);
+        vector.add(300);
+
+        vector.get(0);
+
+        System.out.println("----------------------------------------------------------------");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(100);
+        stack.add(200);
+        stack.add(300);
+
+        System.out.println(stack);
+
+        int lastElement = stack.pop();
+
+        System.out.println(stack);
+
+        int secondLastElement = stack.pop();
+
+        System.out.println(secondLastElement);
 
 
 
